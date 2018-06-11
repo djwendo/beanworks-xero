86b867c2fb75fa63fc7403ae13077e488db12782
djwendo <wendocalrissian@gmail.com>
2018-06-11 09:43:26 -0700

    Pull data from Xero API for Accounts, Invoices, Contacts

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-
 import logo from './logo.svg';
-
 import './App.css';
+
 
 class App extends Component {
   state = {
-    response: ''
+    invoices: [],
   };
 
   componentDidMount() {
+    console.log('fetch invoices')
+    // fetch('/invoices')
+    //   .then(res => res.json())
+    //   .then(invoices => this.setState({ invoices }))
+    //   console.log('invoices', this.state.invoices);
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ invoices: [] }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+    const invoices = await fetch('/invoices');
+    console.log('response', invoices);
+    const body = await invoices.json();
+    console.log('body', body)
 
-    if (response.status !== 200) throw Error(body.message);
+    if (invoices.status !== 200) throw Error(body.message);
 
     return body;
   };
@@ -31,7 +37,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        {/* <h1>Invoices</h1>
+        {this.state.invoices.map(invoice =>
+          <div key={invoice.invoiceID}>{invoice.contact.name}</div>
+        )} */}
       </div>
     );
   }
--- server.js
+++ server.js
@@ -3,8 +3,50 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+const XeroClient = require('xero-node').AccountingAPIClient;
+const config = require('./config.json');
+
+let xero = new XeroClient(config);
+
+let invoiceList;
+
+let accountList;
+
+let vendorList;
+ 
+(async () => {
+  
+  invoiceList = await xero.invoices.get();
+  return invoiceList;
+
+})();
+
+(async () => {
+ 
+  accountList = await xero.accounts.get();
+  return accountList;
+
+})();
+
+(async () => {
+ 
+  vendorList = await xero.contacts.get();
+  return vendorList;
+
+})();
+
+
+
+app.get('/invoices', (req, res) => {
+  res.send([ invoiceList ]);
+});
+
+app.get('/accounts', (req, res) => {
+  res.send([ accountList ]);
+});
+
+app.get('/contacts', (req, res) => {
+  res.send([ vendorList ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
